29bab0b19cef8f77632e6a8e7f95e646386a66b1
Oliver Galloway-Lunn <olivergl@akvo.org>
2013-10-24 13:08:07 +0100

    [#99, #107] Added wrapper script for synchronising deleted project updates.

---
+++ scripts/sync_sites_updates.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SYNC_SITES_UPDATES_SCRIPT="http://akvofoundation.org/syncUpdates.php"
+LOG_TIME_STAMP=`date +%Y%m%d_%H%M%S`
+LOG_FILE_NAME="sync_updates_log_$LOG_TIME_STAMP.html"
+LOG_FILE="/var/log/akvo/sites/$LOG_FILE_NAME"
+LOG_FILE_URL="http://akvofoundation.org/log/sites/$LOG_FILE_NAME"
+
+printf "<br />[`date --rfc-3339=seconds`]<br />" >> $LOG_FILE
+curl $SYNC_SITES_UPDATES_SCRIPT 2>&1 >> $LOG_FILE
+printf "<br />" >> $LOG_FILE
+
+echo $LOG_FILE_URL | mail -s '[sites.akvo.org] Akvo Sites project update deletions synchronised' devops@akvo.org
