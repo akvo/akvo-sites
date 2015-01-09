ee0e7b5d44ad9625dc4d340749aa0e4bf1f2e606
Eveline Sparreboom <eveline@kominski.net>
2015-01-09 16:06:32 +0100

    network plugin cleanup. Removed unused plugins, added network plugin "plugin activation status" which checks if there are inactive plugins

--- plugins/wsi/wsi/back/actions/ResetAction.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Vérification du token de sécurité.
-check_admin_referer('reset','nonce_reset_field');
-
-// Remise de toutes les options aux valeurs par défaut
-MainManager::getInstance()->reset();
-
-?>
